591d123b01f57860e7a479c197f8e27ebb7f0744
Elie Roux <roux.elie@gmail.com>
2025-10-10 17:29:31 +0200

    0.8.1 for Jena 4.10

--- src/main/java/io/bdrc/jena/sttl/TurtleShell.java
+++ src/main/java/io/bdrc/jena/sttl/TurtleShell.java
@@ -62,7 +62,6 @@ import org.apache.jena.riot.out.NodeToLabel;
 import org.apache.jena.riot.system.PrefixMap;
 import org.apache.jena.riot.system.PrefixMapFactory;
 import org.apache.jena.riot.system.RiotLib;
-import org.apache.jena.riot.writer.DirectiveStyle;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.sparql.util.Context;
@@ -132,7 +131,7 @@ public class TurtleShell {
     }
 
     protected void writeBase(final String base) {
-        RiotLib.writeBase(out, base, DirectiveStyle.KEYWORD);
+        RiotLib.writeBase(out, base, true);
     }
 
     // returns the number of prefixes written
